f2c2ecdacb70352b81ef06525b392c319de8dad9
Millie Ho <millie.yun@gmail.com>
2017-07-01 17:40:33 +0800

    Basic calculator for bumper plates.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import Calculator from './Calculator.jsx';
 
 ReactDOM.render(
-    <App />, document.getElementById('app'));
+    <Calculator />, document.getElementById('app'));
