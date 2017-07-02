108a018e6a2568032be1238d2de8519362a85a69
Millie Ho <millie.yun@gmail.com>
2017-07-02 16:18:09 +0800

    Add Material ui

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Calculator from './Calculator.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const App = () => (
+    <MuiThemeProvider>
+        <Calculator/>
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-    <Calculator/>, document.getElementById('app'));
+    <App/>, document.getElementById('app'));
