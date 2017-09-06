756b6109b65e5db32cb4cc816da309c09df1e2e5
Millie Ho <millie.yun@gmail.com>
2017-09-06 09:04:27 +0800

    Add Ga

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import './scss/main.scss';
+import Ga from './ga.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Calculator from './Calculator.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
 injectTapEventPlugin();
 
 ReactDOM.render(
